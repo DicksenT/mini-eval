1c3738f4846e8acb375e4925d2022d7e2d8b5b5b
Dicksen Tan <tandicksen@gmail.com>
2025-11-25 03:35:08 +0700

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,11 @@
-import { Input } from "@/components/input";
+import { Evaluator } from "@/components/Evaluator";
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center">
+    <main className="flex justify-center items-center h-screen w-screen">
       <h1>Mini Evaluator</h1>
       <h2>JavaScript's Code</h2>
-       <Input/>
+       <Evaluator/>
     </main>
   );
 }
